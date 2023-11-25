4f90076fb9d686b0c7169b59ac4e0d38cbde3c62
Kohei Akiyama <kohei.akiyama@outlook.jp>
2023-11-26 04:13:16 +0900

    修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  constructor() {
+    effect(() => {
+      console.log('count changed', this.count());
+    });
+  }
   title = 'angular-sample-app';
+
+  count = signal(0);
+  doubleCount = computed(() => this.count() * 2);
+
+  increment() {
+    this.count.update((value) => value + 1);
+  }
+
+  set3() {
+    this.count.set(3);
+  }
 }
